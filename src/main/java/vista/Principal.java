b58737b1131f0b3ad9e5fd5717e364a8c40bbda8
ochoa <ochoa@172.16.214.162>
2022-11-16 11:47:08 -0500

    Unidad.03.01.Ventanas.MVC

---
+++ src/main/java/vista/Principal.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ochoa
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+        var ventana0301a = new JFrame();
+        ventana0301a.setTitle("Ventana Unidad 03.01a");
+        ventana0301a.setSize(500, 500);
+        ventana0301a.setLocation(200, 100);
+        ventana0301a.setVisible(true);
+        ventana0301a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         var ventana0301b = new JFrame();
+        ventana0301b.setTitle("Ventana Unidad 03.01b");
+        ventana0301b.setSize(500, 500);
+        ventana0301b.setLocation(300, 200);
+        ventana0301b.setVisible(true);
+        ventana0301b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         var ventana0301c = new JFrame();
+        ventana0301c.setTitle("Ventana Unidad 03.01c");
+        ventana0301c.setSize(500, 500);
+        ventana0301c.setLocation(400, 300);
+        ventana0301c.setVisible(true);
+        ventana0301c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         var ventana0301d = new JFrame();
+        ventana0301d.setTitle("Ventana Unidad 03.01d");
+        ventana0301d.setSize(500, 500);
+        ventana0301d.setLocation(500, 400);
+        ventana0301d.setVisible(true);
+        ventana0301d.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
